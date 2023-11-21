f8b8f47bf6479137210a1c8af92012a950865e16
Zhongshi Luo <bdssrojack@gmail.com>
2023-11-21 13:33:37 -0500

    Refactored ParticipantImpl operate() and doCommit() for thread safety.

--- src/main/java/ParticipantImpl.java
+++ src/main/java/ParticipantImpl.java
@@ -66,18 +66,18 @@ public class ParticipantImpl {
             String key = request.getKey();
             if (o == Operation.GET) {
                 Response response;
-                if (store.containsKey(key)) {
-                    response = Response.newBuilder().setStatus(true).setMsg(MessageLib.GET_SUCCEED(key, store.get(key))).build();
+                // Thread safety: no further operation after get()
+                String value = store.get(key);
+                if (value != null) {
+                    // key value pair exists
+                    response = Response.newBuilder().setStatus(true).setMsg(MessageLib.GET_SUCCEED(key, value)).build();
                 } else {
+                    // no such key in store
                     response = Response.newBuilder().setStatus(false).setMsg(MessageLib.GET_FAILED(key)).build();
                 }
                 responseObserver.onNext(response);
                 responseObserver.onCompleted();
                 logger.log(response);
-                return;
-            } else if (o == Operation.DELETE && !store.containsKey(key)) {
-                responseObserver.onNext(Response.newBuilder().setStatus(false).setMsg(MessageLib.GET_FAILED(key)).build());
-                responseObserver.onCompleted();
                 return;
             } else if (o == Operation.UNRECOGNIZED) {
                 responseObserver.onNext(Response.newBuilder().setStatus(false).setMsg(MessageLib.INVALID_OPERATION).build());
@@ -105,60 +105,66 @@ public class ParticipantImpl {
          */
         @Override
         public void canCommit(Trans transaction, StreamObserver<Response> responseObserver) {
-            logger.logInfo(String.format("Preparing %s [%s].", MessageLib.REQUEST(transaction.getRequest()),transaction.getTid().getTid()));
+            logger.logInfo(String.format("Preparing %s [%s].", MessageLib.REQUEST(transaction.getRequest()), transaction.getTid().getTid()));
             // store the request in temporary storage
             tmp.put(transaction.getTid().getTid(), transaction.getRequest());
             // always return true as simplification
             responseObserver.onNext(Response.newBuilder().setStatus(true).build());
             responseObserver.onCompleted();
-            logger.logInfo(String.format("Prepared %s [%s].", MessageLib.REQUEST(transaction.getRequest()),transaction.getTid().getTid()));
+            logger.logInfo(String.format("Prepared %s [%s].", MessageLib.REQUEST(transaction.getRequest()), transaction.getTid().getTid()));
         }
 
         /**
          * Call from coordinator to tell all participants to commit its part of a transaction.
          */
         @Override
-        public void doCommit(Tid tid, StreamObserver<Response> responseObserver){
+        public void doCommit(Tid tid, StreamObserver<Response> responseObserver) {
             Request request = tmp.get(tid.getTid());
             tmp.remove(tid.getTid());
             Response response = null;
             Operation o = request.getOperation();
             String key = request.getKey(), value = request.getValue();
 
-            logger.logInfo(String.format("Committing %s [%s].", MessageLib.REQUEST(request),tid.getTid()));
+            logger.logInfo(String.format("Committing %s [%s].", MessageLib.REQUEST(request), tid.getTid()));
 
             switch (o) {
                 case PUT -> {
                     if (value.isBlank() || value.isEmpty()) {
                         response = Response.newBuilder().setStatus(false).setMsg(MessageLib.UPDATE_FAILED(key)).build();
-                    } else if (store.containsKey(key)) {
-                        store.put(key, value);
-                        response = Response.newBuilder().setStatus(true).setMsg(MessageLib.UPDATE_SUCCEED(key, value)).build();
                     } else {
-                        store.put(key, value);
-                        response = Response.newBuilder().setStatus(true).setMsg(MessageLib.ADD_SUCCEED(key, value)).build();
+                        // Thread safety: no further operation after put()
+                        String preValue = store.put(key, value);
+                        if (preValue != null) {
+                            // Updated existing tuple with new value
+                            response = Response.newBuilder().setStatus(true).setMsg(MessageLib.UPDATE_SUCCEED(key, value)).build();
+                        } else {
+                            // No previous value associated with the key, new tuple added
+                            response = Response.newBuilder().setStatus(true).setMsg(MessageLib.ADD_SUCCEED(key, value)).build();
+                        }
                     }
                 }
                 case DELETE -> {
-                    if (store.containsKey(key)) {
-                        value = store.get(key);
-                        store.remove(key);
+                    // Thread safety: no further operation after remove()
+                    value = store.remove(key);
+                    if (value != null) {
+                        // key value pair removed
                         response = Response.newBuilder().setStatus(true).setMsg(MessageLib.DELETE_SUCCEED(key, value)).build();
                     } else {
+                        // no such key value pair in store
                         response = Response.newBuilder().setStatus(false).setMsg(MessageLib.GET_FAILED(key)).build();
                     }
                 }
             }
             responseObserver.onNext(response);
             responseObserver.onCompleted();
-            logger.logInfo(String.format("Committed %s [%s].", MessageLib.REQUEST(request),tid.getTid()));
+            logger.logInfo(String.format("Committed %s [%s].", MessageLib.REQUEST(request), tid.getTid()));
         }
 
         /**
          * Call from coordinator to tell participant to abort its part of a transaction
          */
         @Override
-        public void doAbort(Tid tid, StreamObserver<Response> responseObserver){
+        public void doAbort(Tid tid, StreamObserver<Response> responseObserver) {
 
         }
     }
